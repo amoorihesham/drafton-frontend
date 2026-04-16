d8c0eae1b05ac8e2186120526193fadf536e98bd
Amr_Hesham <amrheshammacc@gmail.com>
2026-04-16 18:56:44 +0200

    solve after register redirected to verify

--- src/app/auth/sign-up/components/sign-up-form.tsx
+++ src/app/auth/sign-up/components/sign-up-form.tsx
@@ -25,7 +25,6 @@ export function SignUpForm() {
 
       if (response.success) {
         toast.success(response.message);
-        router.push(`/auth/verify-email?email=${response.data.email}`);
       } else {
         toast.error(response.error?.code, { description: response.error?.message });
       }
